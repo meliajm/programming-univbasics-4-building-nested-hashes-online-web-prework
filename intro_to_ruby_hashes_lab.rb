d3bf311b0efefda5adbe432a70edb32a274b70a7
melia miller <meliajm@gmail.com>
2019-11-24 19:04:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,8 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {railroads: {pieces: 4,
-              rent_in_dollars: {one_piece_owned: 25, b: "", c: "", d: ""}, 
+              rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50, three_piece_owned: 75, 
+              four_piece_owned: 100}, 
               names: {a: "", b: "", c: "", d: ""}
     }
   }
